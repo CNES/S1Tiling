87408c9deb8038e17df6450e18ea1d40b274d84e
Luc Hermitte <luc.hermitte@c-s.fr>
2020-03-17 18:58:55 +0000

    Add first N test
    For the moment the tests lives directly on a specific disk. It'll be correctly
    versionned later.

---
+++ tests/test_0200306-NR.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import pathlib
+
+def process(cwd):
+    '''
+    Executes the S1Processor
+    '''
+    crt_dir = pathlib.Path(__file__).parent.absolute()
+    src_dir = crt_dir.parent.absolute()
+    args=[src_dir/'S1Processor.py', 'test.cfg']
+    print(args)
+    return subprocess.call(args, cwd=cwd)
+
+def otb_compare(baseline, result):
+    """
+    Compare the images produced by the test
+    """
+    args=['otbTestDriver', '--compare-image', '1e-12', baseline, result, 'Execute', 'echo', '"running OTB Compare"', '-testenv']
+    print(args)
+    return subprocess.call(args)
+
+def test_0200306_NR():
+    images = ['33NWB/s1a_33NWB_vh_DES_007_20200108txxxxxx.tif', '33NWB/s1a_33NWB_vv_DES_007_20200108txxxxxx.tif']
+    cwd = '/work/scratch/hermittel/dev/S1Tiling/tests/20200306-NR/'
+    process(cwd)
+    baseline_path = cwd+'data_baseline_out/'
+    result_path   = cwd+'data_out2/'
+    for im in images:
+        assert otb_compare(baseline_path+im, result_path+im) == 0
+    # The following line permits to test otb_compare correctly detect differences when called from pytest.
+    # assert otb_compare(baseline_path+images[0], result_path+images[1]) == 0
