5251fe7fe423415332c657f10ad5ce7ad512d85e
Luc Hermitte <luc.hermitte@c-s.fr>
2020-03-19 14:45:57 +0000

    Clear previous test results before testing again

--- tests/test_0200306-NR.py
+++ tests/test_0200306-NR.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import pathlib
+import os
+import shutil
 from helpers import otb_compare
 
 def process(cwd):
@@ -11,16 +13,20 @@ def process(cwd):
     crt_dir = pathlib.Path(__file__).parent.absolute()
     src_dir = crt_dir.parent.absolute()
     args=[src_dir/'S1Processor.py', 'test.cfg']
+    # TODO: extract the result path from the config file
+    result_path   = cwd+'data_out2/'
+    if os.path.exists(result_path):
+         shutil.rmtree(result_path)
     print(args)
-    return subprocess.call(args, cwd=cwd)
+    return subprocess.call(args, cwd=cwd), result_path
 
 
 def test_0200306_NR():
     images = ['33NWB/s1a_33NWB_vh_DES_007_20200108txxxxxx.tif', '33NWB/s1a_33NWB_vv_DES_007_20200108txxxxxx.tif']
     cwd = '/work/scratch/hermittel/dev/S1Tiling/tests/20200306-NR/'
-    process(cwd)
+    EX, result_path = process(cwd)
+    assert EX == 0
     baseline_path = cwd+'data_baseline_out/'
-    result_path   = cwd+'data_out2/'
     for im in images:
         assert otb_compare(baseline_path+im, result_path+im) == 0
     # The following line permits to test otb_compare correctly detect differences when called from pytest.
