c1dbb57ae319752c3af8fbebe3657b305fd6c2b1
Luc Hermitte <luc.hermitte@csgroup.eu>
2020-11-25 20:56:33 +0100

    TST: Make testing more flexible
    Add parameters to be able to test in any configuration

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import pathlib
+import argparse
+
+# - ${S1TILING_TEST_DATA_OUTPUT}
+# - ${S1TILING_TEST_DATA_INPUT}
+# - ${S1TILING_TEST_SRTM}
+# - ${S1TILING_TEST_TMPDIR}
+# - ${S1TILING_TEST_DOWNLOAD}
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return pathlib.Path(path)
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not a valid directory")
+
+def pytest_addoption(parser):
+    crt_dir = pathlib.Path(__file__).parent.absolute()
+    src_dir = crt_dir.parent.absolute()
+    test_dir = (crt_dir.parent.parent / "tests").absolute()
+
+    parser.addoption("--baselinedir", action="store",      default=crt_dir/'baseline',                 type=dir_path, help="Directory where the baseline is")
+    parser.addoption("--outputdir",   action="store",      default=crt_dir/'output',                   type=dir_path, help="Directory where the S2 products will be generated. Don't forget to clean it eventually.")
+    parser.addoption("--tmpdir",      action="store",      default=crt_dir/'tmp',                      type=dir_path, help="Directory where the temporary files will be generated. Don't forget to clean it eventually.")
+    parser.addoption("--srtmdir",     action="store",      default=os.getenv('SRTM_DIR', '$SRTM_DIR'), type=dir_path, help="Directory where SRTM files are - default: $SRTM_DIR")
+    parser.addoption("--download",    action="store_true", default=False, help="Download the input files with eodag instead of using the compressed ones from the baseline. If true, raw S1 products will be downloaded into {tmpdir}/inputs")
+
+def pytest_generate_tests(metafunc):
+    # This is called for every test. Only get/set command line arguments
+    # if the argument is specified in the list of test "fixturenames".
+    option_list = ['baselinedir', 'srtmdir', 'download', 'outputdir', 'tmpdir']
+    for option in option_list:
+        value = getattr(metafunc.config.option, option)
+        # print("%s ===> %s // %s" % (option, value, option in metafunc.fixturenames))
+        # value = metafunc.config.option.baselinedir
+        if option in metafunc.fixturenames and value is not None:
+            metafunc.parametrize(option, [value])
