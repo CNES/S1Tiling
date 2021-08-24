052653cac347f505ec6be5a7c904ce3889af6fb8
Luc Hermitte <9-lhermitte@users.noreply.gitlab.orfeo-toolbox.org>
2021-08-24 13:20:39 +0000

    Resolve "Provide a ready to use S1Tiling in a docker"

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# export LANG=en_US.utf8
+# RuntimeError: Click will abort further execution because Python was
+# configured to use ASCII as encoding for the environment. Consult
+# https://click.palletsprojects.com/unicode-support/ for mitigation
+# steps.
+# This system supports the C.UTF-8 locale which is recommended. You
+# might be able to resolve your issue by exporting the following
+# environment variables:
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+. "${OTB_INSTALL_DIRNAME}/otbenv.profile"
+. "${S1TILING_VENV}/bin/activate"
+S1Processor "$@"
