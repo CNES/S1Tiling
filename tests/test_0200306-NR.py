0536d0e8c3888be84434d2a3ef618d22b4c32151
Luc Hermitte <luc.hermitte@csgroup.eu>
2022-04-28 18:07:58 +0200

    TST: Fix mocking over manifest.safe access

--- tests/test_0200306-NR.py
+++ tests/test_0200306-NR.py
@@ -13,7 +13,7 @@ from unittest.mock import patch
 
 # import pytest_check
 from .helpers import otb_compare, comparable_metadata
-from .mock_otb import OTBApplicationsMockContext, isfile, isdir, list_dirs, glob, dirname
+from .mock_otb import OTBApplicationsMockContext, isfile, isdir, list_dirs, glob, dirname, makedirs
 import s1tiling.S1Processor
 import s1tiling.libs.configuration
 
@@ -194,7 +194,9 @@ class FileDB:
                 's1_polarless'        : 's1a-iw-grd-20200108t044150-20200108t044215-030704-038506',
                 's2_polarless'        : 's1a_33NWB_DES_007_20200108t044150',
                 'dem_coverage'        : ['N00E014', 'N00E015', 'N00E016', 'N01E014', 'N01E015', 'N01E016', 'N02E014', 'N02E015', 'N02E016'],
-                # 'polygon'     : [(14.233953, 1.137156), (16.461103, 0.660935), (16.77552, 2.173307), (14.545785, 2.645077), (14.233953, 1.137156)]
+                'polygon'             : [(1.137156, 14.233953), (0.660935, 16.461103), (2.173307, 16.77552), (2.645077, 14.545785), (1.137156, 14.233953)],
+                'orbit_direction'     : 'DES',
+                'relative_orbit'      : 7,
                 },
             {
                 's1dir'               : 'S1A_IW_GRDH_1SDV_20200108T044215_20200108T044240_030704_038506_D953',
@@ -203,7 +205,9 @@ class FileDB:
                 's1_polarless'        : 's1a-iw-grd-20200108t044215-20200108t044240-030704-038506',
                 's2_polarless'        : 's1a_33NWB_DES_007_20200108t044215',
                 'dem_coverage'        : ['N00E013', 'N00E014', 'N00E015', 'N00E016', 'N01E014', 'S01E013', 'S01E014', 'S01E015', 'S01E016'],
-                # 'polygon'     : [(13.917268, -0.370174), (16.143845, -0.851051), (16.461084, 0.660845), (14.233407, 1.137179), (13.917268, -0.370174)]
+                'polygon'             : [(-0.370174, 13.917268), (-0.851051, 16.143845), (0.660845, 16.461084), (1.137179, 14.233407), (-0.370174, 13.917268)],
+                'orbit_direction'     : 'DES',
+                'relative_orbit'      : 7,
                 },
             # 20 jan 2020
             {
@@ -213,7 +217,9 @@ class FileDB:
                 's1_polarless'        : 's1a-iw-grd-20200120t044214-20200120t044239-030879-038B2D',
                 's2_polarless'        : 's1a_33NWB_DES_007_20200120t044214',
                 'dem_coverage'        : ['N00E013', 'N00E014', 'N00E015', 'N00E016', 'N01E014', 'S01E013', 'S01E014', 'S01E015', 'S01E016'],
-                # 'polygon'             : [(13.917237, -0.370036), (16.143806, -0.850946), (16.461067, 0.660948), (14.233396, 1.137315), (13.917237, -0.370036)]
+                'polygon'             : [(-0.370036, 13.917237), (-0.850946, 16.143806), (0.660948, 16.461067), (1.137315, 14.233396), (-0.370036, 13.917237)],
+                'orbit_direction'     : 'DES',
+                'relative_orbit'      : 7,
                 },
             {
                 's1dir'               : 'S1A_IW_GRDH_1SDV_20200120T044149_20200120T044214_030879_038B2D_5671',
@@ -222,18 +228,28 @@ class FileDB:
                 's1_polarless'        : 's1a-iw-grd-20200120t044149-20200120t044214-030879-038B2D',
                 's2_polarless'        : 's1a_33NWB_DES_007_20200120T044149',
                 'dem_coverage'        : ['N00E014', 'N00E015', 'N00E016', 'N01E014', 'N01E015', 'N01E016', 'N02E014', 'N02E015', 'N02E016'],
-                # 'polygon'             : [(14.233942, 1.137292), (16.461086, 0.661038), (16.775522, 2.173408), (14.545794, 2.645211), (14.233942, 1.137292)]
+                'polygon'             : [(1.137292, 14.233942), (0.661038, 16.461086), (2.173408, 16.775522), (2.645211, 14.545794), (1.137292, 14.233942)],
+                'orbit_direction'     : 'DES',
+                'relative_orbit'      : 7,
                 },
             ]
     CONCATS = [
             # 08 jan 2020
             {
-                's2_basename':  's1a_33NWB_{polarity}_DES_007_20200108txxxxxx',
+                's2_basename' :  's1a_33NWB_{polarity}_DES_007_20200108txxxxxx',
                 's2_polarless': 's1a_33NWB_DES_007_20200108txxxxxx',
-                # 'concatlia':
-                # 'concatsinlia':
+                'first_date'  : '2020-01-01',
+                'last_date'   : '2020-01-10',
+                },
+            # 20 jan 2020
+            {
+                's2_basename' :  's1a_33NWB_{polarity}_DES_007_20200120txxxxxx',
+                's2_polarless': 's1a_33NWB_DES_007_20200120txxxxxx',
+                'first_date'  : '2020-01-10',
+                'last_date'   : '2020-01-21',
                 },
             ]
+    TILE = '33NWB'
     extended_geom_compress = '?&writegeom=false&gdal:co:COMPRESS=DEFLATE'
     extended_compress      = '?&gdal:co:COMPRESS=DEFLATE'
 
@@ -327,12 +343,48 @@ class FileDB:
         assert idx < 2
         return self.input_file(idx, polarity='vv')
 
+    def tile_origins(self, tile_name):
+        origins = {
+                '33NWB': [(14.9998201759, 1.8098185887), (15.9870050338, 1.8095484335), (15.9866155411, 0.8163071941), (14.9998202469, 0.8164290331000001)],
+                }
+        return origins[tile_name]
+
     def raster_vv(self, idx):
+        S2_tile_origin = self.tile_origins(self.TILE)
         s1dir  = self.FILES[idx]['s1dir']
-        return (S1DateAcquisition(
-            f'{self.__input_dir}/{s1dir}/{s1dir}.SAFE/manifest.safe',
-            [self.input_file(idx, polarity='vv')]),
-            [(14.9998201759, 1.8098185887), (15.9870050338, 1.8095484335), (15.9866155411, 0.8163071941), (14.9998202469, 0.8164290331000001)])
+        coverage = compute_coverage(FILES[idx]['polygon'], S2_tile_origin)
+        logging.debug("coverage of %s by %s = %s", self.TILE, s1dir, coverage)
+        return {
+                'raster': S1DateAcquisition(f'{self.safe_dir(idx)}/manifest.safe', [self.input_file_vv(idx)]),
+                'tile_origin': S2_tile_origin,
+                'tile_coverage': coverage
+                }
+
+    def _find_image(self, manifest_path):
+        manifest_path = str(manifest_path)  # manifest_path is either a str or a PosixPath
+        for idx in range(len(self.FILES)):
+            if self.FILES[idx]['s1dir'] in manifest_path:
+                return idx
+        raise AssertionError(f'{manifest_path} cannot be found in input list {[f["s1dir"] for f in self.FILES]}')
+
+    def get_origin(self, manifest_path):
+        """
+        Mock alternative for Utils.get_origin
+        """
+        idx = self._find_image(manifest_path)
+        origin = self.FILES[idx]['polygon'][1:]
+        logging.debug('  mock.get_origin(%s) -> %s', self.FILES[idx]['s1dir'], origin)
+        return origin
+
+    def get_orbit_direction(self, manifest_path):
+        idx = self._find_image(manifest_path)
+        dir = self.FILES[idx]['orbit_direction']
+        return dir
+
+    def get_relative_orbit(self, manifest_path):
+        idx = self._find_image(manifest_path)
+        dir = self.FILES[idx]['relative_orbit']
+        return dir
 
     def cal_ok(self, idx, tmp, polarity='vv'):
         crt = self.FILES[idx]
@@ -359,7 +411,7 @@ class FileDB:
         crt = self.FILES[idx]
         return self._concatfile_for_all(crt, tmp, polarity)
     def concatfile_from_two(self, idx, tmp, polarity='vv'):
-        crt = self.CONCATS[idx // 2]
+        crt = self.CONCATS[idx]
         return self._concatfile_for_all(crt, tmp, polarity)
 
     def _masktmp_for_all(self, crt, tmp, polarity):
@@ -473,16 +525,23 @@ def _declare_know_files(mocker, known_files, known_dirs, patterns, file_db):
         files += [fn for fn in all_files if fnmatch.fnmatch(fn, '*'+pattern+'*')]
     known_files.extend(files)
     known_dirs.update([dirname(fn, 3) for fn in known_files])
+    known_dirs.update([dirname(fn, 2) for fn in known_files])
+    # known_dirs.update([dirname(fn, 1) for fn in known_files])
     logging.debug('Mocking w/ %s --> %s', patterns, files)
     # Utils.list_dirs has been imported in S1FileManager. This is the one that needs patching!
     mocker.patch('s1tiling.libs.S1FileManager.list_dirs', lambda dir, pat : list_dirs(dir, pat, known_dirs, file_db.inputdir))
-    mocker.patch('glob.glob',        lambda pat : glob(pat, known_files))
+    mocker.patch('glob.glob',        lambda pat  : glob(pat, known_files))
     mocker.patch('os.path.isfile',   lambda file : isfile(file, known_files))
+    mocker.patch('os.path.isdir',    lambda dir  : isdir(dir, known_dirs))
+    mocker.patch('os.makedirs',      lambda dir, **kw  : makedirs(dir, known_dirs))
     mocker.patch('os.path.getctime', lambda file : 0)
     # TODO: Test written meta data as well
     mocker.patch('s1tiling.libs.otbwrappers.OrthoRectify.add_ortho_metadata',    lambda slf, mt, app : True)
     mocker.patch('s1tiling.libs.otbwrappers.OrthoRectifyLIA.add_ortho_metadata', lambda slf, mt, app : True)
     mocker.patch('s1tiling.libs.otbpipeline.commit_execution',                   lambda tmp, out : True)
+    mocker.patch('s1tiling.libs.Utils.get_origin',          lambda manifest : file_db.get_origin(manifest))
+    mocker.patch('s1tiling.libs.Utils.get_orbit_direction', lambda manifest : file_db.get_orbit_direction(manifest))
+    mocker.patch('s1tiling.libs.Utils.get_relative_orbit',  lambda manifest : file_db.get_relative_orbit(manifest))
 
     def mock_commit_execution_for_SelectLIA(inp, out):
         logging.debug('mock.mv %s %s', inp, out)
@@ -763,7 +822,6 @@ def test_33NWB_202001_lia_mocked(baselinedir, outputdir, tmpdir, srtmdir, ram, d
             dryrun=False, debug_otb=True, watch_ram=False,
             debug_tasks=False)
     application_mocker.assert_all_have_been_executed()
-
 
 
 def test_33NWB_202001_normlim_mocked(baselinedir, outputdir, tmpdir, srtmdir, ram, download, watch_ram, mocker):
