a30d55c232de53eb85f607fb30f1ea61abe9dfa5
fabien contival <fabien.contival@csgroup.eu>
2024-06-26 10:58:44 +0200

    Add input keys for RigidTransformResample mocking

--- tests/mock_otb.py
+++ tests/mock_otb.py
@@ -41,7 +41,22 @@ from unittest import TestCase
 
 # WARNING: Update these lists everytime an OTB application with an original
 # naming scheme for its parameters is used.
-k_input_keys  = ['io.in', 'in', 'il', 'in.normals', 'in.xyz', 'insar', 'indem', 'indemproj', 'xyz', 'inr', 'inm']
+k_input_keys  = [
+    'io.in',
+    'in',
+    'il',
+    'in.normals',
+    'in.xyz',
+    'insar',
+    'indem',
+    'indemproj',
+    'xyz',
+    'inr',
+    'inm',
+    'transform.type',
+    'transform.type.id.scalex',
+    'transform.type.id.scaley'
+]
 k_output_keys = ['io.out', 'out', 'out.lia', 'out.sin', 'out.gamma_area']
 
 
