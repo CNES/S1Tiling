3b7a4427596d00725dfa575ac7796b16a8cc7208
Luc Hermitte <luc.hermitte@csgroup.eu>
2024-09-21 19:43:16 +0200

    WIP-ENH: Add SentinelOrbitFile class + service
    for searching, filtering...

---
+++ s1tiling/libs/orbit/_file.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =========================================================================
+#   Program:   S1Processor
+#
+#   All rights reserved.
+#   Copyright 2017-2024 (c) CNES.
+#
+#   This file is part of S1Tiling project
+#       https://gitlab.orfeo-toolbox.org/s1-tiling/s1tiling
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# =========================================================================
+#
+# Authors:
+# - Thierry KOLECK (CNES)
+# - Luc HERMITTE (CSGROUP)
+#
+# =========================================================================
+
+""" This sub-module defines the Sentinel1 Orbit file class """
+
+from collections.abc import Sequence
+from datetime import datetime
+import glob
+import logging
+import os
+from pathlib import Path
+from typing import List, Tuple, Union
+
+from eof.download import SentinelOrbit
+
+from ._conversions import ORBIT_CONVERTERS
+from ..utils import lxml as xml
+
+
+logger = logging.getLogger('s1tiling.orbit')
+
+class SentinelOrbitFile(SentinelOrbit):
+    """
+    Extends :class:`eof.SentinelOrbit` with min-max absolute orbit info
+    """
+    def __init__(self, filename: Union[str, Path], **kwargs) -> None:
+        """
+        constructor
+        """
+        super().__init__(filename, **kwargs)
+        assert self.mission in ORBIT_CONVERTERS, (
+                f"Unexpected mission ID {self.mission!r}. Only {ORBIT_CONVERTERS.keys()} are supported."
+        )
+
+        self.first_abs_orbit, self.last_abs_orbit = extract_min_max_abs_orbit_numbers(filename)
+
+        self.__orbit_converter = ORBIT_CONVERTERS[self.mission]
+        self.first_rel_orbit = self.__orbit_converter.to_relative(self.first_abs_orbit)
+        self.last_rel_orbit  = self.__orbit_converter.to_relative(self.last_abs_orbit)
+
+    def does_intersect(self, start: datetime, stop: datetime) -> bool:
+        """
+        Tells whether an EOF file intersect the given time range
+        """
+        return self.start_time < stop and self.stop_time > start
+
+    def has_relative_orbit(self, relative_orbit: int, margin: int = 0) -> bool:
+        """
+        Tells whether a relative orbit is stored in a EOF file
+        """
+        min = self.first_rel_orbit - margin
+        max = self.last_rel_orbit + margin
+        if min < max:
+            return min <= relative_orbit <= max
+        else:
+            # min is close to 175, and max is close to 0
+            return (min <= relative_orbit <= 175) or (1 <= relative_orbit <= max)
+
+
+def extract_min_max_abs_orbit_numbers(filename: Union[str, Path]) -> Tuple[int, int]:
+    # ~ 80ms with lxml, 2.7s with xml
+    root = xml.parse(filename)
+    if not root:
+        raise RuntimeError(f"Cannot open EOF file: {filename!r}")
+    osv_list = xml.find(
+            root,
+            "Data_Block/List_of_OSVs",
+            str(filename) + "<Earth_Explorer_File/>",
+    );
+
+    min_obt = xml.find_as(
+            int,
+            osv_list,
+            "OSV/Absolute_Orbit",
+            filename,
+    )
+    max_obt = xml.find_as(
+            int,
+            osv_list,
+            "OSV[last()]/Absolute_Orbit",
+            filename,
+    )
+    return int(min_obt), int(max_obt)
+
+
+def glob_eof_files(dirname: Union[str, Path]) -> List[SentinelOrbitFile]:
+    eof_files = sorted([
+        SentinelOrbitFile(f)
+        for f in glob.glob(os.path.join(dirname, "S1*OPER_AUX_POEORB*.EOF"))
+    ])
+    return eof_files
+
+
+def filter_intersecting_eof_files(
+        eof_files : List[SentinelOrbitFile],
+        first_date: datetime,
+        last_date : datetime,
+        missions  : Sequence[str] = (),
+) -> List[SentinelOrbitFile]:
+    if missions:
+        return [
+                f for f in eof_files
+                if f.does_intersect(first_date, last_date) and f.mission in missions
+        ]
+    else:
+        return [
+                f for f in eof_files
+                if f.does_intersect(first_date, last_date)
+        ]
+
+
+def filter_eof_files_containing_orbit(
+        eof_files     : List[SentinelOrbitFile],
+        relative_orbit: int,
+) -> List[SentinelOrbitFile]:
+    return [ f for f in eof_files if f.has_relative_orbit(relative_orbit)]
+
