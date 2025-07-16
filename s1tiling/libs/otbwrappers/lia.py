23746aa1551c89f24ece6755b1fa68759d4c7420
Luc Hermitte <luc.hermitte@cs-soprasteria.com>
2025-07-16 15:32:34 +0200

    BUG: Revert future change on extended_filename_ia

--- s1tiling/libs/otbwrappers/lia.py
+++ s1tiling/libs/otbwrappers/lia.py
@@ -1053,12 +1053,7 @@ class _ComputeIncidenceAngle(OTBStepFactory):
                 default_disable_streaming = otb_version() < '9.1.1'
                 params_out        .append(f'out.{ia_map.name}')
                 fname_fmts        .append(TemplateOutputFilenameGenerator(fname_fmt))
-                extended_filenames.append(
-                    extended_filename_ia(
-                        cfg,
-                        ia_map,
-                        cfg.disable_streaming.get('normals_on_s2', default_disable_streaming) and incidence_angle_kind == "LIA"
-                    ))
+                extended_filenames.append(extended_filename_ia(cfg, ia_map))
                 pixel_types       .append(pixel_type_ia(cfg, ia_map, incidence_angle_kind))
                 self.__data_types .append(self._data_type_fmts[ia_map].format(IA=incidence_angle_kind))
                 image_description .append(image_description_dict[ia_map])
