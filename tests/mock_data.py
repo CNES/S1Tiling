7aaf4fe40d616ce804684a08b5b75c2c88571a53
Luc Hermitte <luc.hermitte@cs-soprasteria.com>
2025-09-18 16:34:36 +0200

    TST-REFACTOR: Make naming more generic in tests

--- tests/mock_data.py
+++ tests/mock_data.py
@@ -38,6 +38,7 @@ import numpy as np
 
 from s1tiling.libs.Utils import Polygon
 from s1tiling.libs.otbtools import otb_version
+from s1tiling.libs.utils.formatters import ExtendedFormatter
 
 # from .mock_otb import compute_coverage
 
@@ -62,11 +63,11 @@ class FileDB:
         's1file'              : '{s1_basename}.tiff',
         'cal_ok'              : '{s1_basename}{tmp}.tiff',
         'ortho_ready'         : '{s1_basename}_OrthoReady{tmp}.tiff',
-        'orthofile'           : '{s2_basename}{calibration}{tmp}',
-        'sigma0_normlim_file' : '{s2_basename}_NormLim{tmp}',
-        'gamma0_rtc_file'     : '{s2_basename}_GammaNaughtRTC{tmp}',
-        'border_mask_tmp'     : '{s2_basename}{calibration}_BorderMaskTmp{tmp}.tif',
-        'border_mask'         : '{s2_basename}{calibration}_BorderMask{tmp}.tif',
+        'orthofile'           : '{s2_basename}{tmp}',
+        # 'sigma0_normlim_file' : '{s2_basename}_NormLim{tmp}',         # no longer needed
+        # 'gamma0_rtc_file'     : '{s2_basename}_GammaNaughtRTC{tmp}',  # no longer needed
+        'border_mask_tmp'     : '{s2_basename}_BorderMaskTmp{tmp}.tif',
+        'border_mask'         : '{s2_basename}_BorderMask{tmp}.tif',
 
         # Local Incidence Angle V1 & γ Area RTC
         'vrt'                 : 'DEM_{s1_polarless}{tmp}.vrt',
@@ -120,7 +121,6 @@ class FileDB:
             'orbit_stop'      : '2020:01:08 23:59:59',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200108T044150_20200108T044215_030704_038506_C7F5',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200108t044150-20200108t044215-030704-038506-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200108t044150',
             's1_polarless'    : 's1a-iw-grd-20200108t044150-20200108t044215-030704-038506',
             's2_polarless'    : 's1a_33NWB_DES_007_20200108t044150',
             'dem_coverage'    : ['N00E014', 'N00E015', 'N00E016', 'N01E014', 'N01E015', 'N01E016', 'N02E014', 'N02E015', 'N02E016'],
@@ -130,6 +130,7 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 30704,
             'orbit'           : '007',
+            'time_stamp'      : '20200108t044150',
         },
         {
             'start_time'      : '2020:01:08 04:42:15',
@@ -138,7 +139,6 @@ class FileDB:
             'orbit_stop'      : '2020:01:08 23:59:59',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200108T044215_20200108T044240_030704_038506_D953',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200108t044215-20200108t044240-030704-038506-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200108t044215',
             's1_polarless'    : 's1a-iw-grd-20200108t044215-20200108t044240-030704-038506',
             's2_polarless'    : 's1a_33NWB_DES_007_20200108t044215',
             'dem_coverage'    : ['N00E013', 'N00E014', 'N00E015', 'N00E016', 'N01E014', 'S01E013', 'S01E014', 'S01E015', 'S01E016'],
@@ -148,6 +148,7 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 30704,
             'orbit'           : '007',
+            'time_stamp'      : '20200108t044215',
         },
         # 20 jan 2020
         {
@@ -157,7 +158,6 @@ class FileDB:
             'orbit_stop'      : '2020:01:20 23:59:59',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200120T044149_20200120T044214_030879_038B2D_5671',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200120t044149-20200120t044214-030879-038B2D-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200120t044149',
             's1_polarless'    : 's1a-iw-grd-20200120t044149-20200120t044214-030879-038B2D',
             's2_polarless'    : 's1a_33NWB_DES_007_20200120t044149',
             'dem_coverage'    : ['N00E014', 'N00E015', 'N00E016', 'N01E014', 'N01E015', 'N01E016', 'N02E014', 'N02E015', 'N02E016'],
@@ -167,6 +167,7 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 30879,
             'orbit'           : '007',
+            'time_stamp'      : '20200120t044149',
         },
         {
             'start_time'      : '2020:01:20 04:42:14',
@@ -175,7 +176,6 @@ class FileDB:
             'orbit_stop'      : '2020:01:20 23:59:59',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200120T044214_20200120T044239_030879_038B2D_FDB0',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200120t044214-20200120t044239-030879-038B2D-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200120t044214',
             's1_polarless'    : 's1a-iw-grd-20200120t044214-20200120t044239-030879-038B2D',
             's2_polarless'    : 's1a_33NWB_DES_007_20200120t044214',
             'dem_coverage'    : ['N00E013', 'N00E014', 'N00E015', 'N00E016', 'N01E014', 'S01E013', 'S01E014', 'S01E015', 'S01E016'],
@@ -185,6 +185,7 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 30879,
             'orbit'           : '007',
+            'time_stamp'      : '20200120t044214',
         },
         # 02 feb 2020
         {
@@ -194,7 +195,6 @@ class FileDB:
             'orbit_stop'      : '2020:02:01 23:59:59',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200201T044149_20200201T044214_031054_039149_ED12',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200201t044149-20200201t044214-031054-039149-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200201t044149',
             's1_polarless'    : 's1a-iw-grd-20200201t044149-20200201t044214-031054-039149',
             's2_polarless'    : 's1a_33NWB_DES_007_20200201t044149',
             'dem_coverage'    : ['N00E014', 'N00E015', 'N00E016', 'N01E014', 'N01E015', 'N01E016', 'N02E014', 'N02E015', 'N02E016'],
@@ -204,13 +204,13 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 31054,
             'orbit'           : '007',
+            'time_stamp'      : '20200201t044149',
         },
         {
             'start_time'      : '2020:02:01 04:42:14',
             'stop_time'       : '2020:02:01 04:42:39',
             's1dir'           : 'S1A_IW_GRDH_1SDV_20200201T044214_20200201T044239_031054_039149_CC58',
             's1_basename'     : 's1a-iw-grd-{polarity}-20200201t044214-20200201t044239-031054-039149-{nr}',
-            's2_basename'     : 's1a_33NWB_{polarity}_DES_007_20200201t044214',
             's1_polarless'    : 's1a-iw-grd-20200201t044214-20200201t044239-031054-039149',
             's2_polarless'    : 's1a_33NWB_DES_007_20200201t044214',
             'dem_coverage'    : ['N00E013', 'N00E014', 'N00E015', 'N00E016', 'N01E014', 'S01E013', 'S01E014', 'S01E015', 'S01E016'],
@@ -220,31 +220,42 @@ class FileDB:
             'relative_orbit'  : 7,
             'absolute_orbit'  : 31054,
             'orbit'           : '007',
+            'time_stamp'      : '20200201t044214',
         },
     ]
     CONCATS = [
         # 08 jan 2020
         {
-            's2_basename' : 's1a_33NWB_{polarity}_DES_007_20200108txxxxxx',
-            's2_polarless': 's1a_33NWB_DES_007_20200108txxxxxx',
-            'start_time'  : '2020:01:08 04:41:50',
-            'orbit'       : '007',
+            's2_polarless'    : 's1a_33NWB_DES_007_20200108txxxxxx',
+            'start_time'      : '2020:01:08 04:41:50',
+            'orbit'           : '007',
+            'orbit_direction' : 'DES',
+            'time_stamp'      : '20200108txxxxxx',
         },
         # 20 jan 2020
         {
-            's2_basename' : 's1a_33NWB_{polarity}_DES_007_20200120txxxxxx',
-            's2_polarless': 's1a_33NWB_DES_007_20200120txxxxxx',
-            'start_time'  : '2020:01:20 04:41:49',
-            'orbit'       : '007',
+            's2_polarless'    : 's1a_33NWB_DES_007_20200120txxxxxx',
+            'start_time'      : '2020:01:20 04:41:49',
+            'orbit'           : '007',
+            'orbit_direction' : 'DES',
+            'time_stamp'      : '20200120txxxxxx',
         },
         # 02 feb 2020
         {
-            's2_basename' : 's1a_33NWB_{polarity}_DES_007_20200201txxxxxx',
-            's2_polarless': 's1a_33NWB_DES_007_20200201txxxxxx',
-            'start_time'  : '2020:02:01 04:41:49',
-            'orbit'       : '007',
+            's2_polarless'    : 's1a_33NWB_DES_007_20200201txxxxxx',
+            'start_time'      : '2020:02:01 04:41:49',
+            'orbit'           : '007',
+            'orbit_direction' : 'DES',
+            'time_stamp'      : '20200201txxxxxx',
         },
     ]
+
+    S2_BASENAME = {
+        'with_calibration': 's1a_33NWB_{polarity}_{orbit_direction}_{orbit}_{time_stamp}{calibration}',
+
+        # here calibration is _sig/_NormLim -> hence :.4 instead of :.3
+        'theia'           : 'S1A_L1ORT_33NWB_{polarity!u}{calibration!u:.4}_{orbit_direction}_{orbit}_{time_stamp}',
+    }
 
     # TILE = '33NWB'
     TILE_DATA = {
@@ -285,6 +296,8 @@ class FileDB:
         self.__dem_dir         = demdir
         self.__GeoidFile       = geoid_file
         self.__dname_fmt_tiled = dname_fmt_tiled
+
+        self.__naming_policy   = 'with_calibration'
 
         NFiles   = len(self.FILES)
         NConcats = len(self.CONCATS)
@@ -405,6 +418,14 @@ class FileDB:
         # for idx in range(NFiles):
         #     self.__tmp_to_out_map[self.orthofile(idx, True, calibration='_beta')] = self.orthofile(idx, False, calibration='_beta')
         #     self.__tmp_to_out_map[self.concatfile_from_one(idx, True, calibration='_beta')] = self.concatfile_from_one(idx, False, calibration='_beta')
+
+    def change_naming_policy(self, naming: str):
+        assert naming in self.S2_BASENAME
+        self.__naming_policy = naming
+
+    @property
+    def s2_basename(self) -> str:
+        return self.S2_BASENAME[self.__naming_policy]
 
     @property
     def tmp_to_out_map(self):
@@ -608,11 +629,18 @@ class FileDB:
     def orthofile(self, idx, tmp, polarity='vv', calibration='_sigma') -> str:
         crt = self.FILES[idx]
         ext = self.extended_geom_compress if tmp else ''
-        return f'{self.__tmp_dir}/S2/{self.__tile}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
+        formatter = ExtendedFormatter()
+        return formatter.format(
+            f'{self.__tmp_dir}/S2/{self.__tile}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
+                **crt,
+                s2_basename=self.s2_basename,
+                tmp=tmp_suffix(tmp),
+                calibration=calibration
+            ),
             **crt,
-            tmp=tmp_suffix(tmp),
+            polarity=polarity,
             calibration=calibration
-        ).format(polarity=polarity)
+        )
 
     # ----------[ concat
     def _concatfile_for_all(self, crt, tmp, polarity, calibration) -> str:
@@ -626,14 +654,18 @@ class FileDB:
             # logging.error('concatfile_for_all(tmp=%s, calibration=%s) ==> OUT ==> %r', tmp, calibration, dir)
         ext = self.extended_compress_predictor if tmp else ''
         assert 'orbit' in crt, f'"orbit" not in {crt.keys()}'
-        return f'{dir}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
+        formatter = ExtendedFormatter()
+        return formatter.format(
+            f'{dir}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
                 **crt,
+                s2_basename=self.s2_basename,
                 tmp=tmp_suffix(tmp),
-                calibration=calibration,
                 out_dir=self.__output_dir,
                 tile_name=self.__tile,
-        ).format(
-                polarity=polarity, nr="001" if polarity == "vv" else "002"
+            ),
+            **crt,
+            calibration=calibration,
+            polarity=polarity, nr="001" if polarity == "vv" else "002",
         )
     def concatfile_from_one(self, idx, tmp, polarity='vv', calibration='_sigma') -> str:
         crt = self.FILES[idx]
@@ -646,13 +678,28 @@ class FileDB:
         crt = self.CONCATS[idx]
         ext = self.extended_compress_predictor if tmp else ''
         return f'{dir}/{self.FILE_FMTS["orthofile"]}{extra}.tif{ext}'.format(
-                **crt, tmp=tmp_suffix(tmp), calibration=calibration).format(
-                        polarity=polarity, nr="001" if polarity == "vv" else "002"
-                )
+            **crt,
+            s2_basename=self.s2_basename,
+            tmp=tmp_suffix(tmp),
+        ).format(
+            **crt,
+            calibration=calibration,
+            polarity=polarity, nr="001" if polarity == "vv" else "002",
+        )
 
     def _masktmp_for_all(self, crt, tmp, polarity, calibration) -> str:
         dir = f'{self.__tmp_dir}/S2/{self.__tile}'
-        return f'{dir}/{self.FILE_FMTS["border_mask_tmp"]}.tif'.format(**crt, tmp=tmp_suffix(tmp), calibration=calibration).format(polarity=polarity)
+        formatter = ExtendedFormatter()
+        return formatter.format(
+            f'{dir}/{self.FILE_FMTS["border_mask_tmp"]}.tif'.format(
+                **crt,
+                s2_basename=self.s2_basename,
+                tmp=tmp_suffix(tmp),
+            ),
+            **crt,
+            calibration=calibration,
+            polarity=polarity,
+        )
     def masktmp_from_one(self, idx, tmp, polarity='vv', calibration='_sigma') -> str:
         crt = self.FILES[idx]
         return self._masktmp_for_all(crt, tmp, polarity, calibration)
@@ -667,7 +714,17 @@ class FileDB:
         else:
             dir = f'{self.__output_dir}/{self.__tile}'
             ext = ''
-        return f'{dir}/{self.FILE_FMTS["border_mask"]}{ext}'.format(**crt, tmp=tmp_suffix(tmp), calibration=calibration).format(polarity=polarity)
+        formatter = ExtendedFormatter()
+        return formatter.format(
+            f'{dir}/{self.FILE_FMTS["border_mask"]}{ext}'.format(
+                **crt,
+                s2_basename=self.s2_basename,
+                tmp=tmp_suffix(tmp),
+            ),
+            **crt,
+            calibration=calibration,
+            polarity=polarity
+        )
     def maskfile_from_one(self, idx, tmp, polarity='vv', calibration='_sigma') -> str:
         crt = self.FILES[idx]
         return self._maskfile_for_all(crt, tmp, polarity, calibration)
@@ -779,7 +836,7 @@ class FileDB:
     def eof_for_s2(self) ->  str:
         return f'{self.__eof_dir}/{self.TILE_DATA[self.__tile]["eof"]}'
 
-    def relorb_for_s2(self) -> str:
+    def relorb_for_s2(self) -> int:
         return self.TILE_DATA[self.__tile]["relorb"]
 
     def dems_on_s2(self) -> List[str]:
@@ -869,7 +926,15 @@ class FileDB:
         else:
             dir = f'{self.__output_dir}/{self.__tile}'
             ext = ''
-        return f'{dir}/{self.FILE_FMTS["sigma0_normlim_file"]}.tif{ext}'.format(**crt, tmp=tmp_suffix(tmp)).format(polarity=polarity)
+        return f'{dir}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
+            **crt,
+            s2_basename=self.s2_basename,
+            tmp=tmp_suffix(tmp),
+        ).format(
+            **crt,
+            calibration='_NormLim',
+            polarity=polarity,
+        )
     def sigma0_normlim_file_from_one(self, idx, tmp, polarity='vv') -> str:
         crt = self.FILES[idx]
         return self._sigma0_normlim_file_for_all(crt, tmp, polarity)
@@ -885,7 +950,15 @@ class FileDB:
         else:
             dir = f'{self.__output_dir}/{self.__tile}'
             ext = ''
-        return f'{dir}/{self.FILE_FMTS["gamma0_rtc_file"]}.tif{ext}'.format(**crt, tmp=tmp_suffix(tmp)).format(polarity=polarity)
+        return f'{dir}/{self.FILE_FMTS["orthofile"]}.tif{ext}'.format(
+            **crt,
+            s2_basename=self.s2_basename,
+            tmp=tmp_suffix(tmp),
+        ).format(
+            **crt,
+            calibration='_GammaNaughtRTC',
+            polarity=polarity,
+        )
     def gamma0_rtc_file_from_one(self, idx, tmp, polarity='vv') -> str:
         crt = self.FILES[idx]
         return self._gamma0_rtc_file_for_all(crt, tmp, polarity)
