742a8c68e887a27a358457cb3285cba3be06ad57
Luc Hermitte <luc.hermitte@cs-soprasteria.com>
2025-11-19 18:55:03 +0100

    TST: Update mocked signature

--- tests/step_defs/test_file_detection.py
+++ tests/step_defs/test_file_detection.py
@@ -554,7 +554,7 @@ def when_searching_VH(configuration, image_list) -> None:
 # ----------------------------------------------------------------------
 # When / download scenarios
 
-def mock_download_one_product(dag, raw_directory, dl_wait, dl_timeout, logging, product) -> S1DownloadOutcome:
+def mock_download_one_product(dag, raw_directory, dl_wait, dl_timeout, logging, sanatize, product) -> S1DownloadOutcome:
     logging.debug('mock: download1 -> %s', product)
     return S1DownloadOutcome(product, product)
 
